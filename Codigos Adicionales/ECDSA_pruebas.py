5d4abafb9f051f42d420798bd7f2f45f4868915c
Andres-Saldana <andres01saldana@gmail.com>
2023-06-16 20:51:52 -0600

    final

--- Codigos Adicionales/ECDSA_pruebas.py
+++ Codigos Adicionales/ECDSA_pruebas.py
@@ -3,7 +3,7 @@
 Curvas elípticas y las funciones requeridas
 para hacer las operaciones necesarias
 
-Esta versión es la que se utilizara para ejecución de la solución
+Esta versión del archivo es en la que se iban haciendo pruebas de las funciones conforme se iban haciendo
 '''
 
 import hashlib
@@ -11,8 +11,6 @@ import binascii
 import secrets
 import time
 import csv
-from Crypto.PublicKey import ECC
-from cryptography import x509
 
 
 #Calcular el Maximo comun divisor de dos numeros usan el algoritmo euclidiano extendido
@@ -29,14 +27,20 @@ def GCDExtended(a,b): #a,b son los numeros a encontrar
         y0, y1 = y1, y0 - q * y1
     return a, x0, y0 #a es el GCD
 
+#Prueba de función
+#print(GCDExtended(123,423))
 
 #Calcular el inverso bajo un modulo p
+
 def Inverso_calc(a, Fn): #a es el inverso a encontrar y Fn el mod p
     GCD ,x , y = GCDExtended(a,Fn)
     if GCD != 1:
         return False
     return x % Fn
 
+
+#Prueba de paper
+#Inverso_calc(5,27)
 
 #Calculo del inverso aditivo
 def inverso_adi(x,p): #p es el modulo
@@ -47,6 +51,7 @@ def inverso_adi(x,p): #p es el modulo
     
     
 #Exponencial binaria, facilita las operaciones que se tienen que realizar dada la longitud de los digitos
+#Función realizada gracias a la ayuda de un compañero como guía
 '''
 x es la base
 n es el exponente, la función opera correctamente ÚNICAMENTE cuando n es primo
@@ -77,6 +82,9 @@ def pendiente(x1, y1, x2, y2, a, p):
         return ((y2 + inverso_adi(y1, p)) * expo_binaria(x2 + inverso_adi(x1, p), p)) % p
     
 
+#Prueba del paper de calculo de pendiente
+#print(pendiente(4,7,13,11,1,23))
+
 #Suma de puntos en una curva
 '''
 p es el modulo en el que se esta trabajando 
@@ -92,6 +100,20 @@ def Suma_Curva(x1,y1,x2,y2,m,p):
     return x3,y3
 
 
+x3, y3 = Suma_Curva(0,11,5,1,pendiente(0,11,5,1,2,17),17)
+
+print(f'suma wahoo puntos: ({x3},{y3})')
+
+'''
+Versiones de prueba para corroborar que funciona de manera correcta
+'''
+
+#print(pendiente(4,7,13,11,1,23))
+#print(inverso_adi(13,23))
+#print(Suma_Curva(4,13,7,11,pendiente(4,7,13,11,1,23),23))
+#print((13 + inverso_adi(13,23)) % 23)
+
+
 '''
 Esta función es para el calculo de Q = dG de la referencia usada.
 Para esto se tiene que trabajar en números binarios para que los calculos sean posibles de realizar
@@ -100,6 +122,8 @@ a el coeficiente lineal de la curva eliptica
 n es el escalar a multiplicar
 Implementación del algoritmo double and add, en el reporte se puede encontrar la referencia de su funcionamiento
 '''
+
+#Función realizada gracias a la ayuda de un compañero como guía
 def mult_binaria(x, y, a, p, n):
     n = bin(n)[3:] #bin() da la representación binaria de un integer
     x2 = x
@@ -118,32 +142,34 @@ def mult_binaria(x, y, a, p, n):
     return x2, y2
 
 
-def obtener_llave_privada(): #da la clave privada, que es el escalar para la mult_binaria, utilizada para numeros pseudo-aleatorios
+x,y = mult_binaria(5,1,2,17,13)
+
+print(f'suma wahoo multi: ({x},{y})')
+
+#Prueba para verificar que mult_binaria() genere U = xG, Ux. Uy dadas por el nist
+
+'''
+Gx= int(0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296)
+Gy = int(0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5)
+a = int(0xffffffff00000001000000000000000000000000fffffffffffffffffffffffc)
+p = int(0xffffffff00000001000000000000000000000000ffffffffffffffffffffffff)
+x = int(0xC9AFA9D845BA75166B5C215767B1D6934E50C3DB36E89B127B8A622B120F6721)
+
+Ux, Uy =mult_binaria(x = Gx,y = Gy, a = a, p =p, n = x)
+
+print(hex(Ux))
+print(hex(Uy))
+
+
+print(0x60fed4ba255a9d31c961eb74c6356d68c049b8923b61fa6ce669622e60f29fb6 == Ux)
+print(0x7903FE1008B8BC99A41AE9E95628BC64F2F1B20C2D7E9F5177A3C294D4462299 == Uy)
+'''
+
+
+def obtener_llave_privada(): #da la clave privada, que es el escalar para la mult_binaria
     #El valor escalar es el definido por la curva NIST P - 256
     n = int(0xffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551)
     return int(secrets.randbelow(n-1) + 1)
-
-
-#Obtencion de llave privada del certificado generado
-def llave_privada_cert():
-
-    f = open('C:\\Users\\moeug\\OneDrive\\Documentos\\algebras modernas\\client.key')
-
-    key = ECC.import_key(f.read())
-
-    return int(key.d)
-
-
-#Obtención de llave privada generada por certificado
-
-def llave_publica_cert():
-
-    certificate_file = open("client.crt","rb")
-    cert_to_check = x509.load_pem_x509_certificate(certificate_file.read())
-    gx = cert_to_check.public_key().public_numbers().x
-    gy = cert_to_check.public_key().public_numbers().y
-
-    return int(gx),int(gy)
 
 
 
@@ -170,6 +196,7 @@ def firmar_mensaje(mensaje,llave_priv):
     p,a,b,gx,gy,n = Parametros_curva()
 
     #1.-Generar numero aleatorio "k" 1 < k < n-1
+    #k = int (0xA6E3C57DD01ABE90086538398355DD4C3B17AA873382B0F24D6129493D8AAD60) #K de vector de prueba, descomentar para caso particular
     k = obtener_llave_privada()
 
     #2.- Calcular kG = (x1,y1), clave publica
@@ -200,6 +227,23 @@ def firmar_mensaje(mensaje,llave_priv):
     return (r,m)
 
 
+#Prueba de vectores de la curva NIST P-256
+'''
+Pr, Pm = firmar_mensaje('sample',0xC9AFA9D845BA75166B5C215767B1D6934E50C3DB36E89B127B8A622B120F6721)
+
+print(Pr == int(0xEFD48B2AACB6A8FD1140DD9CD45E81D69D2C877B56AAF991C34D0EA84EAF3716))
+print(Pm == int(0xF7CB1C942D657C41D436C7A1B6E29F65F3E900DBB9AFF4064DC4AB2F843ACDA8))
+'''
+
+'''
+Funcion para la verificación del proceso de firma, los parametros son:
+-r,s la firma previamente calculada
+-G = Gx, Gy los valores de la curva publicos
+-a el coeficiente lineal de la curva a utilizar
+-n el numero primo verificado
+-m el mensaje a vericar
+-Q siendo el calculo de la multiplicacion binaria Q = Ux, Uy
+'''
 def verificar_firma(r, s, m, n, G, Q, a):
 
     #1.-Verificar que r y s son enteros en el intervalos en [1,n-1]
@@ -245,53 +289,45 @@ def verificar_firma(r, s, m, n, G, Q, a):
 
 #Ejemplo de vectores de prueba
 
-p,a,b,gx,gy,n = Parametros_curva()
-x = llave_privada_cert()
-Gx,Gy = llave_publica_cert()
-Ux , Uy = mult_binaria (gx , gy ,a ,p , x)
+p = int (0xffffffff00000001000000000000000000000000ffffffffffffffffffffffff )
+a = int (0xffffffff00000001000000000000000000000000fffffffffffffffffffffffc )
+b = int (0x5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b )
+gx = int (0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296 )
+gy = int (0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5 )
+n = int (0xffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551 )
+#x = int (0xC9AFA9D845BA75166B5C215767B1D6934E50C3DB36E89B127B8A622B120F6721 )
+x = obtener_llave_privada()
+Ux , Uy = mult_binaria ( gx , gy ,a ,p , x )
+
+'''
+r,s = firmar_mensaje('C,02/11/2013,1,58.0', x)
+
+print(hex(r))
+print(hex(s))
+#print(r == int(0xEFD48B2AACB6A8FD1140DD9CD45E81D69D2C877B56AAF991C34D0EA84EAF3716))
+#print(s == int(0xF7CB1C942D657C41D436C7A1B6E29F65F3E900DBB9AFF4064DC4AB2F843ACDA8))
+'''
+
 
 
 '''
 Prueba  de lectura de tramas en intervalos de las tramas
-'''
 
 
-def Leerconsumo():
-    file = open("TramasConsumo.csv", 'r')
+file = open("TramasConsumo.csv", 'r')
 
+text = file.readline()
+start_time = time.time()
+while text !="":
+    #start_time = time.time()
     text = file.readline()
-    start_time = time.time()
-    while text !="":
-        #start_time = time.time()
-        text = file.readline()
-        ' '.join(text)
-        print(text)
-        time.sleep(1)
-        r,s = firmar_mensaje(text,x)
-        verificar_firma(r,s,text,n,(gx,gy),(Ux,Uy),a)
-        #print("--- %s seconds ---" % (time.time() - start_time))
-        print('\n')
-    print("--- %s seconds ---" % (time.time() - start_time))
+    ' '.join(text)
+    print(text)
+    #time.sleep()
+    r,s = firmar_mensaje(text,x)
+    verificar_firma(r,s,text,n,(gx,gy),(Ux,Uy),a)
+    #print("--- %s seconds ---" % (time.time() - start_time))
+    print('\n')
+print("--- %s seconds ---" % (time.time() - start_time))
 
-def LeerProduccion():
-    file = open("TramasProduccion.csv", 'r')
-
-    #text = file.readline()
-    start_time = time.time()
-    while text !="":
-        #start_time = time.time()
-        text = file.readline()
-        ' '.join(text)
-        print(text)
-        time.sleep(1)
-        r,s = firmar_mensaje(text,x)
-        verificar_firma(r,s,text,n,(gx,gy),(Ux,Uy),a)
-        #print("--- %s seconds ---" % (time.time() - start_time))
-        print('\n')
-    print("--- %s seconds ---" % (time.time() - start_time))
-
-#print(Gx == Ux)
-#print(Gy == Uy)
-
-#Leerconsumo()
-#LeerProduccion()
+'''
